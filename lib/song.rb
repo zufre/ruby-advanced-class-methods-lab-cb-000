f3f4c1c9992a5a0f493b1f3c5ab9098ea1e1b57d
Cedric David <cedric.david@btopenworld.com>
2018-09-04 04:33:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
-class Song
+
+  class Song
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -11,33 +12,34 @@ class Song
   end
 
   def self.create
-    song = Song.new
+    song = self.new
     song.save
     song
   end
 
-  def self.new_by_name(song_name)
-    song = self.new
-    song.name = song_name
-    song
-  end
-
-  def self.create_by_name(song_name)
+  def self.new_by_name(name)
     song = self.create
-    song.name = song_name
+    song.name = name
     song
   end
 
-  def self.find_by_name(song_name)
-    self.all.detect{|s| s.name == song_name}
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  def self.find_by_name(name)
+    self.all.find {|p| p.name == name}
   end
 
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    end
+    self.find_by_name(name)
   end
-
   def self.alphabetical
-    self.all.sort_by{|s| s.name}
+    self.all.sort_by {|w| w.name}
   end
 
   def self.new_from_filename(filename)
@@ -63,7 +65,6 @@ class Song
   end
 
   def self.destroy_all
-    self.all.clear
+    @@all.clear
   end
-
 end
